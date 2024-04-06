7b346fe9068cc785ee0cddacb5acceb6565f3141
zwer9 <ktlusty9@gmail.com>
2024-04-07 01:12:54 +0200

    Dodajemy pierwsza lekcje

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Kocham Kamisia!");
+        char a = 'a';
+
+        String kamilPierdzi = "Kamil pierdzi wszystko smierdzi";
+
+        int anniversary = 2307;
+        System.out.println(anniversary + " " + anniversary);
+
+        int kamilsBday = 0204;
+        float myBday = 1005.00F;
+        double year = 2024;
+        short day = 7;
+        long temp = -546766;
+        boolean kamilIsTheHandsomest = true;
+        String bearKamis = "Kamis to kochany mis";
+        char w = 'w';
+        var duck = "duck";
+        System.out.println("Rok: " + year +
+                "\nMoje urodziny: " + myBday +
+                "\nUrodziny Kamisia: " + kamilsBday +
+                "\nDzien: " + day +
+                "\nTemperatura: " + temp +
+                "\nCzy Kamis jest najpiękniejszy na świecie? " + kamilIsTheHandsomest +
+                "\nLitera imienia Weronisi" + bearKamis);
+    }
+}
